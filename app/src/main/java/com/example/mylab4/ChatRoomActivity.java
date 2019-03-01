d6215bcc8cc4c2e106d61cf1892ad0ce5d172208
Hank <zhou0153@algonquinlive.com>
2019-03-01 13:32:58 -0500

    lab4

---
+++ app/src/main/java/com/example/mylab4/ChatRoomActivity.java
@@ -0,0 +1,87 @@
+package com.example.mylab4;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ChatRoomActivity extends AppCompatActivity {
+
+    ListView listView;
+    EditText editText;
+    List<MessageModel> listMessage = new ArrayList<>();
+    Button sendBtn;
+    Button receiveBtn;
+    DatabaseHelper db;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_chatroom);
+
+        listView = (ListView)findViewById(R.id.ListView);
+        editText = (EditText)findViewById(R.id.ChatEditText);
+        sendBtn = (Button)findViewById(R.id.SendBtn);
+        receiveBtn = (Button)findViewById(R.id.ReceiveBtn);
+        db = new DatabaseHelper(this);
+
+
+        viewData();
+
+        sendBtn.setOnClickListener(c -> {
+            String message = editText.getText().toString();
+            if (!message.equals("")){
+//                MessageModel model = new MessageModel(message, true);
+//                listMessage.add(model);
+//
+//                ChatAdapter adt = new ChatAdapter(listMessage, getApplicationContext());
+//                listView.setAdapter(adt);
+                db.insertData(message, true);
+                editText.setText("");
+                listMessage.clear();
+                viewData();
+            }
+        });
+
+        receiveBtn.setOnClickListener(c -> {
+            String message = editText.getText().toString();
+            if (!message.equals("")) {
+//                MessageModel model = new MessageModel(message, false);
+//                listMessage.add(model);
+//                editText.setText("");
+//                ChatAdapter adt = new ChatAdapter(listMessage, getApplicationContext());
+//                listView.setAdapter(adt);
+                db.insertData(message, false);
+                editText.setText("");
+                listMessage.clear();
+                viewData();
+            }
+        });
+        Log.d("ChatRoomActivity","onCreate");
+    }
+
+    private void viewData(){
+        Cursor cursor = db.viewData();
+
+        if (cursor.getCount() != 0){
+            while (cursor.moveToNext()){
+                MessageModel model = new MessageModel(cursor.getString(1), cursor.getInt(2)==0);
+                listMessage.add(model);
+                ChatAdapter adt = new ChatAdapter(listMessage, getApplicationContext());
+                listView.setAdapter(adt);
+
+            }
+        }
+    }
+
+}
+
